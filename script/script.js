061b4cc3a5e3d6875ccc6f1d31625e6d1a728369
charenming <ysem88@gmail.com>
2021-08-24 00:04:40 +0900

    gangwonstar

--- script/script.js
+++ script/script.js
@@ -1,20 +1,31 @@
 
-     $(document).ready(function(){
-         // 대메뉴 중에서 현제 내가 마우스를 올린 대메뉴의 해당 서브메뉴만 내려온다
-         $(".gnb>li").mouseover(function(){
-             // this -> 내가 현제 선택한 대메뉴 위치
-             $(this).children(".submenu").stop().slideDown();
-         });
-         // 대메뉴 영역에서 마우스가 벗어나면 서브메뉴가 올라간다
-         $(".gnb>li").mouseleave(function(){
-             $(this).children(".submenu").stop().slideUp();
-         });
-         
-         // 공지사항 첫번째 글을 클릭하면 레이어 팝업이 뜬다. 
-         $(".click").click(function(){
-             $("#popup_bg").fadeIn();
-         });
-         $(".close").click(function(){
-             $("#popup_bg").fadeOut();
-         });
-     });   
+    $(function(){
+        $(".mainMenu>li").mouseover(function(){
+            $(this).children(".subMenu").stop().slideDown();
+        });
+        $(".mainMenu>li").mouseleave(function(){
+            $(this).children(".subMenu").stop().slideUp();
+        });
+        
+        // 탭메뉴
+        $(".tabMenu>a").click(function(){
+            $(this).addClass("on").siblings("a").removeClass("on");
+            let idx = $(this).index();  console.log(idx);
+            $(".tabContents>ul").eq(idx).show().siblings("ul").hide();
+        });
+    
+        //슬라이드
+        setInterval(function(){
+            $(".slide>li:first").fadeOut(1000).fadeIn(1000);
+            $(".slide>li:first").insertAfter(".slide>li:last");
+        }, 2000);
+        
+        //팝업
+
+        $(".popUpLine").click(function(){
+            $(".popUp").show(); 
+        })
+        $(".close").click(function(){
+            $(".popUp").hide();
+        });
+    });   
